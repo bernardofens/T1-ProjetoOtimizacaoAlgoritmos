75882b982edcca126589ad0a86ea4b2545ba3275
Bernardo Fensterseifer <bernardo.023@edu.pucrs.br>
2026-04-15 22:39:28 +0000

    setup inicial

---
+++ Main.java
@@ -0,0 +1,15 @@
+import util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+
+        s.close()
+
+    }
+
+}
